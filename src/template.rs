89c82eb6a7325aba7a82340acbe80179b29a4879
silentvoid13 <51264226+SilentVoid13@users.noreply.github.com>
2024-12-01 16:15:25 +0100

    chore: adding basic day template

---
+++ src/template.rs
@@ -0,0 +1,35 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+type Input = u32;
+
+#[aoc_generator(dayx)]
+pub fn parse(input: &str) -> Input {
+    0
+}
+
+#[aoc(dayx, part1)]
+pub fn part1(input: &Input) -> u32 {
+    0
+}
+
+#[aoc(dayx, part2)]
+pub fn part2(input: &Input) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE: &str = r#""#;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(&parse(EXAMPLE)), 0);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse(EXAMPLE)), 0);
+    }
+}
