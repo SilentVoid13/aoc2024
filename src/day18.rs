11bbbcb4bcf632d4f951f20e682a7fb4382c21de
silentvoid13 <51264226+SilentVoid13@users.noreply.github.com>
2024-12-18 10:01:10 +0100

    refactor: neat optimization for p2

--- src/day18.rs
+++ src/day18.rs
@@ -6,7 +6,7 @@ use aoc_utils::{
     point::{Point, ORTHO},
 };
 
-type Input = (Grid<u8>, Vec<Point>);
+type Input = (Grid<u32>, Vec<Point>);
 
 #[aoc_generator(day18)]
 pub fn parse(input: &str) -> Input {
@@ -22,16 +22,15 @@ pub fn parse(input: &str) -> Input {
     let mut grid = Grid {
         width,
         height: width,
-        bytes: vec![b'.'; width * width],
+        bytes: vec![u32::MAX; width * width],
     };
-    let sim = if pos.len() < 50 { 12 } else { 1024 };
-    for &pos in pos.iter().take(sim) {
-        grid[pos] = b'#';
+    for (i, &pos) in pos.iter().enumerate() {
+        grid[pos] = i as u32;
     }
     (grid, pos)
 }
 
-fn bfs(grid: &Grid<u8>) -> usize {
+fn bfs(grid: &Grid<u32>, n_fall: u32) -> usize {
     let mut q = VecDeque::new();
     let mut seen = grid.copy_filled(0);
     let start = Point::new(0, 0);
@@ -45,7 +44,7 @@ fn bfs(grid: &Grid<u8>) -> usize {
         }
         for dir in ORTHO {
             let new_p = p + dir;
-            if grid.contains(new_p) && grid[new_p] == b'.' && seen[new_p] == 0 {
+            if grid.contains(new_p) && grid[new_p] > n_fall && seen[new_p] == 0 {
                 seen[new_p] = 1;
                 q.push_back((new_p, score + 1));
             }
@@ -56,23 +55,18 @@ fn bfs(grid: &Grid<u8>) -> usize {
 
 #[aoc(day18, part1)]
 pub fn part1(input: &Input) -> usize {
-    let (grid, _) = input;
-    bfs(grid)
+    let (grid, poss) = input;
+    let sim = if poss.len() < 50 { 12 - 1 } else { 1024 - 1 };
+    bfs(grid, sim)
 }
 
 #[aoc(day18, part2)]
 pub fn part2(input: &Input) -> String {
     let (grid, poss) = input;
-    let sim = if poss.len() < 50 { 12 } else { 1024 };
-    let mut grid = grid.clone();
-    for &point in poss.iter().skip(sim) {
-        grid[point] = b'#';
-        let score = bfs(&grid);
-        if score == usize::MAX {
-            return format!("{},{}", point.x, point.y);
-        }
-    }
-    panic!("not found");
+    let n_falls: Vec<_> = (0..poss.len()).collect();
+    let first_block = n_falls.partition_point(|&v| bfs(grid, v as u32) != usize::MAX);
+    let p = poss[first_block];
+    format!("{},{}", p.x, p.y)
 }
 
 #[cfg(test)]
